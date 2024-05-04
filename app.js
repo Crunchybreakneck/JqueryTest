9586565893071bce88d3424740c7afe737bcaef2
Crunchybreakneck <167829059+Crunchybreakneck@users.noreply.github.com>
2024-05-04 10:56:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ $('#background').click(function(){
   $('body').toggleClass('altBackground')
 $( ".header" ).append( "<p>Test</p>" );
 });
+  $(".text").mouseenter(function(){
+  alert("You found the text!");
+});
 });
