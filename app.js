d9f570a46e978bf59d1e5aea347c1e3e84f1a234
Crunchybreakneck <167829059+Crunchybreakneck@users.noreply.github.com>
2024-05-04 10:54:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,4 @@ $('#background').click(function(){
   $('body').toggleClass('altBackground')
 $( ".header" ).append( "<p>Test</p>" );
 });
-   $("header").keypress(function(){
- $( ".header" ).append( "<p>Key pressed</p>" );
-  });
 });
